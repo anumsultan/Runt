26d16dcfeef11f7ba2fe6f4ae1bd42f53505c28e
anum <anumsultan94@gmail.com>
2022-08-01 18:03:37 +0500

    my website1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false
+    });
+  });
