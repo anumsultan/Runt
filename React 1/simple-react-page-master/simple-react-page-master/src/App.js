3e87d3906e97490c6b0494e10f6d5f170c49ffd6
anum <anumsultan94@gmail.com>
2022-08-23 16:59:55 +0500

    ReactPractice1

---
+++ React 1/simple-react-page-master/simple-react-page-master/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./Header"
+import PageContent from './PageContent';
+import Footer from './Footer';
+
+export default function App(){
+  return(
+    <> 
+      <Header/>
+      <PageContent/>
+      <Footer/>
+    </>
+  )
+}
+
